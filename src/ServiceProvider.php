dc5d732fc7d2b3788afaf097a8b94929253fba4a
Jeroen Noten <jeroennoten@me.com>
2016-08-07 19:00:14 +0200

    typo

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -131,7 +131,7 @@ class ServiceProvider extends BaseServiceProvider
     {
         $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
             $event->menu->add([
-                'text' => 'Nieuwsbrieven vesturen',
+                'text' => 'Nieuwsbrieven versturen',
                 'url' => 'admin/newsletters',
             ], [
                 'text' => 'Verzendlijsten',
